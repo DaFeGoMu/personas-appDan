18bf7fab7a56fb743c9c23074eed6d882895635a
DaFeGoMu <danfgomez@hotmail.com>
2025-11-11 11:53:31 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('comunas', ComunaController::class);
+//Route::get('/comunas', [ComunaController::class, 'index']);
