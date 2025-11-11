d9279ddd5b53dbefbf74be790d850b593d2c69d0
DaFeGoMu <danfgomez@hotmail.com>
2025-11-11 14:14:04 -0500

    delete commune

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -101,6 +101,13 @@ class ComunaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comuna = DB::table('tb_comuna')
+                ->join('tb_municipio', 'tb_comuna.muni_codi', 'tb_municipio.muni.codi')
+                ->select('tb_comuna.*',"tb_municipio.muni_nomb")
+                ->get();
+        return view('comuna.index', ['comunas'=> $comunas]);
     }
 }
